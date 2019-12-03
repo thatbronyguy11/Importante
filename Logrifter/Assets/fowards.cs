ad486fc728d905694e6740b7454bdd2468ed0142
Jackson <Jacksonthebisexual@gmail.com>
2019-12-02 19:40:01 -0500

    FULL DEMO
    this is the full demo
    
    as of 12/2/2019

---
+++ Logrifter/Assets/fowards.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fowards : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        Vector3 xVelocity = rb.velocity;
+        if(xVelocity.x < 0)
+        {
+            xVelocity.x = (xVelocity.x * -1);
+        }
+        if(xVelocity.x <= 20)
+        {
+            xVelocity.x = (xVelocity.x + 2);
+        }
+    }
+}
