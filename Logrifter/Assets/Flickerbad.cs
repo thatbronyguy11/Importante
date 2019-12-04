5b353eb8f5796370486049231f18a03a33f9e146
Jackson <Jacksonthebisexual@gmail.com>
2019-12-03 20:40:12 -0500

    LEDs

---
+++ Logrifter/Assets/Flickerbad.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flickerbad : MonoBehaviour
+{
+    Light light;
+    public float L1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        L1 = Random.Range(1.0f, 60.0f);
+        if (L1 > 58.0)
+        {
+            
+            light.enabled = enabled;   
+            
+        }
+        else
+        {
+            light.enabled = !enabled;
+        }
+        
+    }
+}
---
+++ Logrifter/Assets/ship/Fading.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fading : MonoBehaviour
+{
+
+    private void Update()
+    {
+        var material = GetComponent<Renderer>().material;
+        var color = material.color;
+        if (color.a > 0)
+        {
+            material.color = new Color(color.r, color.g, color.b, color.a + (1f * Time.deltaTime));
+        }
+    }
+}
+
