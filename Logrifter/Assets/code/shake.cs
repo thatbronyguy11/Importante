feb5d4ec9dbf651e3019c39d11ac8e1591320198
Jackson <Jacksonthebisexual@gmail.com>
2019-12-05 18:04:51 -0500

    The LED Room
    look out for unstable electrons!

---
+++ Logrifter/Assets/code/shake.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shake : MonoBehaviour
+{
+
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        animator.enabled = enabled;
+    }
+
+    void OnTriggerLeave(Collider other)
+    {
+        animator.enabled = !enabled;
+    }
+}
