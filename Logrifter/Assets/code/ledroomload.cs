795e189e25bb8356a322f6381ac4ac2e828ef1b7
Jackson <Jacksonthebisexual@gmail.com>
2019-12-10 10:48:33 -0500

    Get an LED

---
+++ Logrifter/Assets/code/ledroomload.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ledroomload : MonoBehaviour
+{
+    //when you be in a colider
+    void OnTriggerStay(Collider other)
+    {
+
+        Application.LoadLevel("LedRoom");
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
